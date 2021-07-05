2074514e6b2ec46cbabd2f2f638f71f8fa381364
Leo Ando <ando.reo420@gmail.com>
2021-07-05 20:48:48 +0900

    芙蓉コードの削除

--- app/src/main/java/com/example/accessibilityviewsample/MainActivity.kt
+++ app/src/main/java/com/example/accessibilityviewsample/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.imageviewsample
+package com.example.accessibilityviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.accessibilityviewsample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
